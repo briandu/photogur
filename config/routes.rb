e5b7cb5c33c74c8b18f61f91c8dcda3beb4c14a0
Brian Du <brian.bh.du@gmail.com>
2017-04-09 20:00:39 -0400

    Photogur 1.0
    Create route,  controller and view for http://localhost:3000/pictures

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pictures' => 'pictures#index'
+
+  # Ignore the comments below for now
+  # They are just documentation
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
